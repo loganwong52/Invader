18684b5ffa9d422dda5054229b3975c7e9707fef
loganwong52 <loganwong52@gmail.com>
2024-01-11 11:36:00 -0800

    score increases when enemy dies

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ bullet = pygame.sprite.GroupSingle()
 enemy_group = pygame.sprite.Group()
 enemy_group.add(Enemy())
 
+# Player score
+score = 0
+
 
 def collision_sprite():
     """
@@ -32,11 +35,13 @@ def collision_sprite():
     pass in Group of sprites that can be collided into
     pass in if the enemy should be deleted or not
     """
+    global bullet, enemy_group, score
     if pygame.sprite.spritecollide(bullet.sprite, enemy_group, True):
         enemy_group.empty()
         bullet.empty()
-        return False
-    return True
+        score += 1
+        # return False
+    # return True
 
 
 def main():
@@ -53,6 +58,9 @@ def main():
     # Timer for bullet
     bullet_timer = pygame.USEREVENT + 1
     pygame.time.set_timer(bullet_timer, 1300)
+
+    # Score
+    font = pygame.font.Font("freesansbold.ttf", 25)
 
     while True:
         for event in pygame.event.get():
@@ -87,6 +95,11 @@ def main():
 
         if bullet.sprite is not None:
             collision_sprite()
+
+        # Show instructions if score is 0, otherwise show the score
+        score_msg = font.render(f"Score: {score}", False, "White")
+        score_msg_rect = score_msg.get_rect(topright=(w, 0))
+        screen.blit(score_msg, score_msg_rect)
 
         # Update everything
         pygame.display.update()
